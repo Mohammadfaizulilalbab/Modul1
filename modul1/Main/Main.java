6b278ac0c4bfa112bb79528d846dad3d64dd8c4a
Mohammadfaizulilalbab <87605628+Mohammadfaizulilalbab@users.noreply.github.com>
2024-02-25 16:08:05 +0700

    Add files via upload

---
+++ modul1/Main/Main.java
@@ -0,0 +1,72 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+// API & Library yang dibutuhkan pada program
+package com.example;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+
+public class Main {
+        public static void main(String[] args) {
+
+
+            // inisiasi variabel-variabel yang diperlukan pada program
+            String nama, jenisKelamin, tglLahirStr;
+            Scanner namaInput = new Scanner(System.in);
+
+
+            System.out.println("-------Inputan user--------\n");
+            // Meminta user untuk menginput nama
+            System.out.print("Masukan nama Anda: ");
+            nama = namaInput.nextLine();
+            // Meminta user untuk menginput jenis kelamin
+            System.out.print("Masukan jenis kelamin Anda (L/P): ");
+            jenisKelamin = namaInput.nextLine();
+            // Meminta user untuk memasukkan tanggal lahir
+            System.out.print("Masukkan tanggal lahir Anda (YYYY-MM-DD): ");
+            tglLahirStr = namaInput.nextLine();
+
+            // Mengonversi string tanggal lahir menjadi objek LocalDate
+            LocalDate tglLahir = LocalDate.parse(tglLahirStr);
+
+            // Menghitung umur berdasarkan tanggal lahir
+            Period usia = Period.between(tglLahir, LocalDate.now());
+
+            // Menampilkan umur
+            System.out.println("Umur Anda adalah: " + usia.getYears() + " tahun, " +
+                    usia.getMonths() + " bulan, dan " +
+                    usia.getDays() + " hari.");
+            // Mengubah input berupa karakter dari pada inputan jenis kelamin dari user menjadi variabel yang bernilai "Laki-laki" atau "Perempuan"
+            switch (jenisKelamin) {
+                case "L":
+                case "l":
+                    jenisKelamin = "Laki-laki";
+                    break;
+                case "P":
+                case "p":
+                    jenisKelamin = "Perempuan";
+                    break;
+                default:
+                    jenisKelamin = "Jenis kelamin lu apa cuy";
+            }
+
+            // Menampilkan seluruh inputan user
+            System.out.println("\n-------Ouput berdasarkan input user--------\n");
+            // Menampilkan nama
+            System.out.println("Nama anda :" + nama);
+            // Menampilkan jenis kelamin
+            System.out.println("Jenis kelamin anda :" + jenisKelamin);
+            // Menampilkan umur
+            System.out.println("Umur Anda adalah: " + usia.getYears() + " tahun, " +
+                    usia.getMonths() + " bulan, dan " +
+                    usia.getDays() + " hari.");
+
+
+        }
+}
+
